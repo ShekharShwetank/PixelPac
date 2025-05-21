286b663f4c88dddeb3f2ce510bcb928b0fefbb21
Vivek Choudhry <126272453+Vivek084c@users.noreply.github.com>
2025-05-21 12:32:39 +0530

    implementing the unet_adaptive_bins later, almost done, encoder block issue

---
+++ models/unet_adaptive_bins.py
@@ -0,0 +1,175 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+# from .miniVit import mViT
+
+import tensorflow as tf
+from tensorflow.keras import layers, Model
+from layers import PatchTransformerEncoder, PixelWiseDotProduct
+
+class mViT(tf.keras.Model):
+    def __init__(self, in_channels, n_query_channels=128, patch_size=16, dim_out=256,
+                 embedding_dim=128, num_heads=4, norm='linear'):
+        super(mViT, self).__init__()
+
+        self.norm = norm
+        self.n_query_channels = n_query_channels
+
+        self.patch_transformer = PatchTransformerEncoder(
+            in_channels=in_channels,
+            patch_size=patch_size,
+            embedding_dim=embedding_dim,
+            num_heads=num_heads
+        )
+
+        self.dot_product_layer = PixelWiseDotProduct()
+
+        self.conv3x3 = layers.Conv2D(
+            filters=embedding_dim,
+            kernel_size=3,
+            strides=1,
+            padding='same'
+        )
+
+        self.regressor = tf.keras.Sequential([
+            layers.Dense(256),
+            layers.LeakyReLU(),
+            layers.Dense(256),
+            layers.LeakyReLU(),
+            layers.Dense(dim_out)
+        ])
+
+    def call(self, x):
+        # x: [batch, height, width, channels]
+        tgt = self.patch_transformer(tf.identity(x))  # [batch, tokens, embedding_dim]
+
+        x = self.conv3x3(x)  # [batch, height, width, embedding_dim]
+
+        regression_head = tgt[:, 0, :]  # [batch, embedding_dim]
+        queries = tgt[:, 1:self.n_query_channels + 1, :]  # [batch, n_query_channels, embedding_dim]
+
+        # Pixel-wise dot product: x is [batch, h, w, embedding_dim], queries is [batch, n_query_channels, embedding_dim]
+        range_attention_maps = self.dot_product_layer(x, queries)  # [batch, n_query_channels, h, w]
+
+        y = self.regressor(regression_head)  # [batch, dim_out]
+
+        if self.norm == 'linear':
+            y = tf.nn.relu(y)
+            eps = 0.1
+            y = y + eps
+        elif self.norm == 'softmax':
+            return tf.nn.softmax(y, axis=1), range_attention_maps
+        else:
+            y = tf.nn.sigmoid(y)
+
+        y = y / tf.reduce_sum(y, axis=1, keepdims=True)
+        return y, range_attention_maps
+
+
+class UpSampleBN(tf.keras.Model):
+    def __init__(self, skip_input, output_features):
+        super(UpSampleBN, self).__init__()
+        self.conv_block = tf.keras.Sequential([
+            layers.Conv2D(output_features, 3, padding='same'),
+            layers.BatchNormalization(),
+            layers.LeakyReLU(),
+            layers.Conv2D(output_features, 3, padding='same'),
+            layers.BatchNormalization(),
+            layers.LeakyReLU()
+        ])
+
+    def call(self, x, concat_with):
+        x_up = tf.image.resize(x, size=tf.shape(concat_with)[1:3], method='bilinear')
+        x_concat = tf.concat([x_up, concat_with], axis=-1)  # TF uses channels_last
+        return self.conv_block(x_concat)
+
+
+class DecoderBN(tf.keras.Model):
+    def __init__(self, num_features=2048, num_classes=1, bottleneck_features=2048):
+        super(DecoderBN, self).__init__()
+        self.conv2 = layers.Conv2D(num_features, 1, padding='same')
+
+        self.up1 = UpSampleBN(num_features + 112 + 64, num_features // 2)
+        self.up2 = UpSampleBN(num_features // 2 + 40 + 24, num_features // 4)
+        self.up3 = UpSampleBN(num_features // 4 + 24 + 16, num_features // 8)
+        self.up4 = UpSampleBN(num_features // 8 + 16 + 8, num_features // 16)
+
+        self.conv3 = layers.Conv2D(num_classes, 3, padding='same')
+
+    def call(self, features):
+        x_block0, x_block1, x_block2, x_block3, x_block4 = features[4], features[5], features[6], features[8], features[11]
+        x_d0 = self.conv2(x_block4)
+        x_d1 = self.up1(x_d0, x_block3)
+        x_d2 = self.up2(x_d1, x_block2)
+        x_d3 = self.up3(x_d2, x_block1)
+        x_d4 = self.up4(x_d3, x_block0)
+        out = self.conv3(x_d4)
+        return out
+
+
+class Encoder(tf.keras.Model):
+    def __init__(self, base_model):
+        super(Encoder, self).__init__()
+        self.base_model = base_model
+
+    def call(self, x):
+        features = [x]
+        for layer in self.base_model.layers:
+            x = layer(x)
+            features.append(x)
+        return features
+
+
+class UnetAdaptiveBins(tf.keras.Model):
+    def __init__(self, encoder, n_bins=100, min_val=0.1, max_val=10.0, norm='linear'):
+        super(UnetAdaptiveBins, self).__init__()
+        
+        self.n_bins = n_bins
+        self.min_val = min_val
+        self.max_val = max_val
+        self.encoder = encoder
+
+        # 💡 Build mViT internally here like in PyTorch
+        self.adaptive_bins_layer = mViT(128, n_query_channels=128, patch_size=16,
+                                        dim_out=n_bins, embedding_dim=128, norm=norm)
+        
+        self.decoder = DecoderBN(num_classes=128)
+
+        self.conv_out = tf.keras.Sequential([
+            layers.Conv2D(n_bins, 1, padding='valid'),
+            layers.Softmax(axis=-1)
+        ])
+
+    def call(self, x):
+        features = self.encoder(x)
+        unet_out = self.decoder(features)
+        bin_widths_normed, range_attention_maps = self.adaptive_bins_layer(unet_out)
+        out = self.conv_out(range_attention_maps)
+
+        bin_widths = (self.max_val - self.min_val) * bin_widths_normed
+        bin_widths = tf.pad(bin_widths, [[0, 0], [1, 0]], constant_values=self.min_val)
+        bin_edges = tf.math.cumsum(bin_widths, axis=1)
+        centers = 0.5 * (bin_edges[:, :-1] + bin_edges[:, 1:])
+        centers = tf.reshape(centers, [-1, self.n_bins, 1, 1])
+
+        pred = tf.reduce_sum(out * centers, axis=1, keepdims=True)
+        return bin_edges, pred
+
+    @classmethod
+    def build(cls, n_bins, input_shape=(480, 640, 3), norm='linear'):
+        base_model = tf.keras.applications.EfficientNetB5(include_top=False, weights='imagenet', input_shape=input_shape)
+        encoder = Encoder(base_model)
+        model = cls(encoder, n_bins=n_bins, norm=norm)
+        print("Model built successfully.")
+        return model
+    
+
+if __name__ == '__main__':
+    model = UnetAdaptiveBins.build(100)
+
+    x = tf.random.normal([2, 480, 640, 3])
+    print(model.adaptive_bins_layer)
+    # bins, pred = model(x)
+
+    # print("Bin edges shape:", bins.shape)
+    # print("Prediction shape:", pred.shape)
